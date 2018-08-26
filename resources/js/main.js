e8efabf7e8345fd4c26847e209dd732972fb6714
Mochi <mwkhrisna@gmail.com>
2018-08-26 10:30:42 +0700

    third commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function(){
-      $('.carousel').slick({
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 4000,
-        arrows: false,
-      });
+  $('.carousel').slick({
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+  });
 
-      var userScroll = 0;
-      $(window).scroll({
-        var userScrollTop = $(this).scrollTop();
-        if (userScrollTop - userScroll > 50) {
-          var navbarHeight = $('.navbar').css('height');
-          $('.navbar').animate({top: '-' +  navbarHeight}, 150);
-          userScroll = userScrollTop;
-        } else if (userScroll - userScrollTop > 50) {
-          $('.navbar').animate({top: '0px'}, 150);
-          userScroll = userScrollTop;
-        };
-      });
-    });
+  var userScroll = 0;
+  $(window).scroll(function(){
+    var userScrollTop = $(this).scrollTop();
+    if (userScrollTop - userScroll > 50) {
+    var navbarHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' +  navbarHeight}, 150);
+     userScroll = userScrollTop;
+    } else if (userScroll - userScrollTop > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+      userScroll = userScrollTop;
+    }
+  });
+});
